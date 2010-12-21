62fc766ea0ae0f8ac094d9f9179b1e3a4539e6c0
pixel <thecatwasnot@gmail.com>
2010-12-21 15:54:13 -0600

    Keyring can add keys and check the length of kit's keyring

--- lib/passwordsafe/keyring.rb
+++ lib/passwordsafe/keyring.rb
@@ -4,10 +4,20 @@ module PasswordSafe
 
     def initialize safe = nil
       @safe = safe
+      @ring = {}
     end
 
     def has_a_safe?
       @safe.is_a? PasswordSafe::Safe
+    end
+
+    def length
+      @ring.length
+    end
+
+    def add name, password
+      raise KeyExistsException, "Key already exists in keyring, if you'd like to add it remove the existing key", caller if @ring.has_key?(name)
+      @ring.store(name, password)
     end
   end
 end
--- spec/keyring_spec.rb
+++ spec/keyring_spec.rb
@@ -8,5 +8,28 @@ describe PasswordSafe::Keyring do
     keyring = PasswordSafe::Keyring.new(PasswordSafe::Safe.new("safefile","masterpass"))
     keyring.should have_a_safe
   end
+
+  context "length" do
+    it "returns the number of keys in the keyring" do
+      keyring = PasswordSafe::Keyring.new(PasswordSafe::Safe.new("safefile","masterpass"))
+      keyring.should respond_to(:length)
+      keyring.length.should be_a(Integer)
+    end
+  end
+
+  context "add" do
+    before(:each) do
+      @keyring = PasswordSafe::Keyring.new(PasswordSafe::Safe.new("safefile","masterpass"))
+    end
+    it "adds a key to the keyring" do
+      @keyring.should respond_to(:add).with(2).arguments
+      @keyring.add("name", "password")
+      @keyring.length.should eq(1)
+    end
+    it "throws an error when adding a duplicate key name" do
+      @keyring.add("name", "password")
+      expect{@keyring.add("name", "password")}.to raise_error()
+    end
+  end
 end
 
