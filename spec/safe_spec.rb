848603a86d8f961039d7a299a1761a4c443cc65c
pixel <thecatwasnot@gmail.com>
2010-12-21 14:05:13 -0600

    Check that no safe file raises an error

--- spec/safe_spec.rb
+++ spec/safe_spec.rb
@@ -7,9 +7,11 @@ describe PasswordSafe::Safe do
     @filename = File.expand_path(@file)
     @masterpass = 'masterpass'
   end
+
   it "takes a safe filename and master password at initilaization" do
     PasswordSafe::Safe.should respond_to(:new).with(2).arguments
   end
+
   context "access_safe" do
     before(:each) do
       @safe = PasswordSafe::Safe.new(@file, @masterpass)
@@ -31,31 +33,38 @@ describe PasswordSafe::Safe do
       @safe = PasswordSafe::Safe.new(@file, @masterpass)
       klass = Class.new { include PasswordSafe::Encryptor}
       @encryptor = klass.new
+      @data = "data to encrypt"
     end
 
     it "creates a safe file to write to" do
-      data = "data to encrypt"
-      @safe.write_safe(data)
+      @safe.write_safe(@data)
       File.file?(@filename).should be_true
     end
 
     it "writes encrypted data to a safe" do
-      data = "data to encrypt"
-      hash = @encryptor.hash(@masterpass)
+      @safe.write_safe(@data)
 
-      @safe.write_safe(data)
-      @encryptor.decrypt(File.read(@filename), hash).should eq(data)
+      #we'll use our encryptor to check the contents
+      hash = @encryptor.hash(@masterpass)
+      @encryptor.decrypt(File.read(@filename), hash).should eq(@data)
     end
   end
+
   context "read_safe" do
     it "reads encrypted data out of an existing safe" do
       @safe = PasswordSafe::Safe.new(@file, @masterpass)
       data = "data to encrypt"
       @safe.write_safe(data)
-      #got data into an existing safe...
+      #got data into an existing safe...now read it out again
       @safe.read_safe.should eq(data)
     end
+
+    it "throws an error if there is no file to read from" do
+      @safe = PasswordSafe::Safe.new(@file, @masterpass)
+      expect{@safe.read_safe}.to raise_error()
+    end
   end
+
   after(:each) do
     #We check on the existance of files when we expect them to exist so this seems ok
     File.delete @filename if File.file?(@filename)
