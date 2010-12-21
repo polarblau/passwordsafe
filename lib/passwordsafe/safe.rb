edb33bc4a573c1cf3926d6c2ef4c8560fd1d564d
pixel <thecatwasnot@gmail.com>
2010-12-21 12:56:48 -0600

    Safe can write encrypted data to a file

--- lib/passwordsafe/safe.rb
+++ lib/passwordsafe/safe.rb
@@ -1,12 +1,19 @@
-
+require 'passwordsafe/encryptor'
 
 module PasswordSafe
   class Safe
+    include PasswordSafe::Encryptor
+
     def initalize filename
     end
 
     def access_safe filename
       FileUtils.touch filename
+    end
+    def write_safe filename, data, hash
+      access_safe filename
+      encrypted_data = encrypt(data, hash)
+      File.open(filename, 'w') {|f| f.write encrypted_data}
     end
   end
 end
--- spec/safe_spec.rb
+++ spec/safe_spec.rb
@@ -24,5 +24,25 @@ describe PasswordSafe::Safe do
       File.delete @filename
     end
   end
+  context "write_safe" do
+    before(:each) do
+      @filename = File.expand_path('passwordsafe')
+      @safe = PasswordSafe::Safe.new()
+      klass = Class.new { include PasswordSafe::Encryptor}
+      @encryptor = klass.new
+    end
+    it "writes data to a safe" do
+      data = "data to encrypt"
+      hash = @encryptor.hash('masterpass')
+      encrypted_data = @encryptor.encrypt(data, hash)
+
+      @safe.write_safe(@filename, data, hash)
+      File.file?(@filename).should be_true
+      @encryptor.decrypt(File.read(@filename), hash).should eq(data)
+    end
+    after(:each) do
+      File.delete @filename
+    end
+  end
 end # describe PasswordSafe::Safe
 
