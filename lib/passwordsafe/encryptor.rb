b3788fa5689d184eda3c40addca2701e22a89139
pixel <thecatwasnot@gmail.com>
2010-12-21 11:39:41 -0600

    Refactor Encryptor to elimiate duplicate code in encrypt/decrypt

--- lib/passwordsafe/encryptor.rb
+++ lib/passwordsafe/encryptor.rb
@@ -5,25 +5,24 @@ module  PasswordSafe
   module Encryptor; extend self;
 
     def decrypt(data, pwhash)
-      cipher = OpenSSL::Cipher.new 'aes-256-cbc'
-      cipher.decrypt
-      cipher.key = pwhash
-      ciphertext = cipher.update(data)
-      ciphertext += cipher.final
+      crypt :decrypt, data, pwhash
     end
 
     def encrypt(data, pwhash)
-      cipher = OpenSSL::Cipher.new 'aes-256-cbc'
-      cipher.encrypt
-      cipher.key = pwhash
-      ciphertext = cipher.update(data)
-      ciphertext += cipher.final
+      crypt :encrypt, data, pwhash
     end
 
     def hash (plain)
       Digest::SHA512.digest(plain)
     end
-  end
 
+    private
+      def crypt(mode, data, pwhash)
+        cipher = OpenSSL::Cipher.new 'AES256'
+        cipher.send mode.to_sym
+        cipher.key = pwhash
+        cipher.update(data) << cipher.final
+      end
+  end
 end
 
