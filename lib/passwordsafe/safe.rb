31bec8112d75fccfaf4734a66352e331e755bc09
pixel <thecatwasnot@gmail.com>
2010-12-21 13:46:56 -0600

    Let the Safe keep track of it's own masterpass hash, just pass the masterpass in on creation

--- lib/passwordsafe/safe.rb
+++ lib/passwordsafe/safe.rb
@@ -4,20 +4,21 @@ module PasswordSafe
   class Safe
     include PasswordSafe::Encryptor
 
-    def initialize filename
+    def initialize filename, masterpass
       @safefile = File.expand_path(filename)
+      @mphash = hash(masterpass)
     end
 
     def access_safe
       FileUtils.touch @safefile
     end
-    def write_safe data, hash
+    def write_safe data
       access_safe
-      encrypted_data = encrypt(data, hash)
+      encrypted_data = encrypt(data, @mphash)
       File.open(@safefile, 'w') {|f| f.write encrypted_data}
     end
-    def read_safe hash
-      decrypt(File.read(@safefile), hash)
+    def read_safe
+      decrypt(File.read(@safefile), @mphash)
     end
   end
 end
--- spec/safe_spec.rb
+++ spec/safe_spec.rb
@@ -5,13 +5,14 @@ describe PasswordSafe::Safe do
   before (:each) do
     @file = 'passwordsafe'
     @filename = File.expand_path(@file)
+    @masterpass = 'masterpass'
   end
-  it "takes a safe filename at initilaization" do
-    PasswordSafe::Safe.should respond_to(:new).with(1).argument
+  it "takes a safe filename and master password at initilaization" do
+    PasswordSafe::Safe.should respond_to(:new).with(2).arguments
   end
   context "access_safe" do
     before(:each) do
-      @safe = PasswordSafe::Safe.new(@file)
+      @safe = PasswordSafe::Safe.new(@file, @masterpass)
     end
     it "creates a safe if none exists" do
       @safe.access_safe
@@ -27,38 +28,32 @@ describe PasswordSafe::Safe do
 
   context "write_safe" do
     before(:each) do
-      @safe = PasswordSafe::Safe.new(@file)
+      @safe = PasswordSafe::Safe.new(@file, @masterpass)
       klass = Class.new { include PasswordSafe::Encryptor}
       @encryptor = klass.new
     end
 
     it "creates a safe file to write to" do
       data = "data to encrypt"
-      hash = @encryptor.hash('masterpass')
-
-      @safe.write_safe(data, hash)
+      @safe.write_safe(data)
       File.file?(@filename).should be_true
     end
 
     it "writes encrypted data to a safe" do
       data = "data to encrypt"
-      hash = @encryptor.hash('masterpass')
+      hash = @encryptor.hash(@masterpass)
 
-      @safe.write_safe(data, hash)
+      @safe.write_safe(data)
       @encryptor.decrypt(File.read(@filename), hash).should eq(data)
     end
   end
   context "read_safe" do
     it "reads encrypted data out of an existing safe" do
-      @safe = PasswordSafe::Safe.new(@file)
+      @safe = PasswordSafe::Safe.new(@file, @masterpass)
       data = "data to encrypt"
-      klass = Class.new { include PasswordSafe::Encryptor}
-      @encryptor = klass.new
-      hash = @encryptor.hash('masterpass')
-      @safe.write_safe(data, hash)
+      @safe.write_safe(data)
       #got data into an existing safe...
-
-      @safe.read_safe(hash).should eq(data)
+      @safe.read_safe.should eq(data)
     end
   end
   after(:each) do
