12eed733b37d91d9ac9227860b84acce88f7b4ab
pixel <thecatwasnot@gmail.com>
2010-12-21 11:16:27 -0600

    Encryptor encrypts strings

--- lib/passwordsafe/encryptor.rb
+++ lib/passwordsafe/encryptor.rb
@@ -1,9 +1,19 @@
+require 'openssl'
+
 module  PasswordSafe
 
   module Encryptor; extend self;
 
+    def encrypt(data, pwhash)
+      cipher = OpenSSL::Cipher.new 'aes-256-cbc'
+      cipher.encrypt
+      cipher.key = pwhash
+      ciphertext = cipher.update(data)
+      ciphertext += cipher.final
+    end
+
     def hash (plain)
-      Digest::SHA512.hexdigest(plain)
+      Digest::SHA512.digest(plain)
     end
   end
 
--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -7,19 +7,36 @@ describe PasswordSafe::Encryptor do
     klass = Class.new { include PasswordSafe::Encryptor}
     @encryptor = klass.new
   end
+  context "hash" do
+    it "creates a password hash" do
+      @encryptor.should respond_to(:hash).with(1).argument
+      @encryptor.hash("test").should be_a(String)
+    end
 
-  it "creates a password hash" do
-    @encryptor.should respond_to(:hash).with(1).argument
-    @encryptor.hash("test").should be_a(String)
+    it "creates the same hash given the same text" do
+      @encryptor.hash("test").should eq(@encryptor.hash("test"))
+    end
+
+    it "creates a different hash given different text" do
+      @encryptor.hash("test").should_not eq(@encryptor.hash("another"))
+    end
   end
+  context "encrypt" do
+    before(:each) do
+      @string = "teststring"
+      @hash = @encryptor.hash("hashstring")
+    end
 
-  it "creates the same hash given the same text" do
-    @encryptor.hash("test").should eq(@encryptor.hash("test"))
+    it "encrypts a string" do
+      @encryptor.should respond_to(:encrypt).with(2).arguments
+      @encryptor.encrypt(@string, @hash).should be_a(String)
+    end
+    it "creates the same string given the same text" do
+      @encryptor.encrypt(@string, @hash).should eq(@encryptor.encrypt(@string, @hash))
+    end
+    it "creates a different string given different text" do
+      @encryptor.encrypt(@string, @hash).should_not eq(@encryptor.encrypt("anotherstring", @hash))
+    end
   end
-
-  it "creates a different hash given different text" do
-    @encryptor.hash("test").should_not eq(@encryptor.hash("another"))
-  end
-
-end
+end # describe PasswordSafe::Encryptor
 
