86e51df1a57cdc833ae550c0d06d1eb891f3214f
pixel <thecatwasnot@gmail.com>
2011-09-13 15:47:55 -0500

    Added specs for CLI.make_safe

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,103 +2,122 @@ require 'spec_helper'
 require 'passwordsafe/cli'
 
 describe PasswordSafe::CLI do
-  let(:dummy_safe) { mock PasswordSafe::Safe }
-  let(:mock_keyring) { mock PasswordSafe::Keyring}
-  before(:each) do
-    subject.stub(:make_safe).and_return(dummy_safe)
-    PasswordSafe::Keyring.stub(:new).with(dummy_safe).and_return(mock_keyring)
-    subject.stub(:puts)
+  context "command interface" do
+    let(:dummy_safe) { mock PasswordSafe::Safe }
+    let(:mock_keyring) { mock PasswordSafe::Keyring}
+    before(:each) do
+      subject.stub(:make_safe).and_return(dummy_safe)
+      PasswordSafe::Keyring.stub(:new).with(dummy_safe).and_return(mock_keyring)
+      subject.stub(:puts)
+    end
+    describe "#add" do
+      it "creates a new key in the keyring" do
+        mock_keyring.should_receive(:add).with("name", "pass")
+        subject.add("name", "pass")
+      end
+      it "displays a KeyExistsException" do
+        mock_keyring.stub(:add) { raise PasswordSafe::Keyring::KeyExistsException, "dummy msg" }
+        subject.should_receive(:puts).with("dummy msg")
+        subject.add("name", "pass")
+      end
+      it "displays a msg to the user on sucessfull add" do
+        mock_keyring.stub(:add)
+        subject.should_receive(:puts).with("password name added to safe")
+        subject.add("name", "pass")
+      end
+    end
+    describe "#generate" do
+      it "generates a password" do
+        mock_keyring.should_receive(:generate)
+        subject.generate("name")
+      end
+      it "displays a KeyExistsException" do
+        mock_keyring.stub(:generate) { raise PasswordSafe::Keyring::KeyExistsException, "dummy msg" }
+        subject.should_receive(:puts).with("dummy msg")
+        subject.generate("name")
+      end
+      it "displays a msg to the user on sucessfull generate" do
+        mock_keyring.stub(:generate).and_return("pass")
+        subject.should_receive(:puts).with("password pass generated and added to safe")
+        subject.generate("name")
+      end
+    end
+    describe "#get" do
+      it "gets the named password from the safe" do
+        mock_keyring.should_receive(:get).with("name")
+        subject.get "name"
+      end
+      it "tells the user if a password does not exist" do
+        mock_keyring.stub(:get).with("name").and_return(nil)
+        subject.should_receive(:puts).with("name does not exist in this safe.")
+        subject.get "name"
+      end
+      it "tells the user the password" do
+        mock_keyring.stub(:get).with("name").and_return("pass")
+        subject.should_receive(:puts).with("name: pass")
+        subject.get "name"
+      end
+    end
+    describe "#remove" do
+      it "removes a key from the keyring" do
+        mock_keyring.should_receive(:remove).with("name")
+        subject.remove("name")
+      end
+      it "displays a KeyMissingException" do
+        mock_keyring.stub(:remove) { raise PasswordSafe::Keyring::KeyMissingException, "dummy msg" }
+        subject.should_receive(:puts).with("dummy msg")
+        subject.remove("name")
+      end
+      it "displays a msg to the user on sucessfull remove" do
+        mock_keyring.stub(:remove)
+        subject.should_receive(:puts).with("entry has been removed")
+        subject.remove("name")
+      end
+    end
+    describe "#change" do
+      it "changes a key in the keyring" do
+        mock_keyring.should_receive(:change).with("name", "pass")
+        subject.change("name", "pass")
+      end
+      it "displays a KeyMissingException" do
+        mock_keyring.stub(:change) { raise PasswordSafe::Keyring::KeyMissingException, "dummy msg" }
+        subject.should_receive(:puts).with("dummy msg")
+        subject.change("name", "pass")
+      end
+      it "displays a msg to the user on sucessfull change" do
+        mock_keyring.stub(:change)
+        subject.should_receive(:puts).with("password for name has been updated")
+        subject.change("name", "pass")
+      end
+    end
+    describe "#list" do
+      it "displays (none) if there are no keys" do
+        mock_keyring.stub(:list).and_return([])
+        subject.should_receive(:puts).with("List: (none)")
+        subject.list
+      end
+      it "displays keys if they exist" do
+        mock_keyring.stub(:list).and_return(["pass1", "pass2"])
+        subject.should_receive(:puts).with("List: pass1, pass2")
+        subject.list
+      end
+    end
   end
-  describe "#add" do
-    it "creates a new key in the keyring" do
-      mock_keyring.should_receive(:add).with("name", "pass")
-      subject.add("name", "pass")
-    end
-    it "displays a KeyExistsException" do
-      mock_keyring.stub(:add) { raise PasswordSafe::Keyring::KeyExistsException, "dummy msg" }
-      subject.should_receive(:puts).with("dummy msg")
-      subject.add("name", "pass")
-    end
-    it "displays a msg to the user on sucessfull add" do
-      mock_keyring.stub(:add)
-      subject.should_receive(:puts).with("password name added to safe")
-      subject.add("name", "pass")
-    end
-  end
-  describe "#generate" do
-    it "generates a password" do
-      mock_keyring.should_receive(:generate)
-      subject.generate("name")
-    end
-    it "displays a KeyExistsException" do
-      mock_keyring.stub(:generate) { raise PasswordSafe::Keyring::KeyExistsException, "dummy msg" }
-      subject.should_receive(:puts).with("dummy msg")
-      subject.generate("name")
-    end
-    it "displays a msg to the user on sucessfull generate" do
-      mock_keyring.stub(:generate).and_return("pass")
-      subject.should_receive(:puts).with("password pass generated and added to safe")
-      subject.generate("name")
-    end
-  end
-  describe "#get" do
-    it "gets the named password from the safe" do
-      mock_keyring.should_receive(:get).with("name")
-      subject.get "name"
-    end
-    it "tells the user if a password does not exist" do
-      mock_keyring.stub(:get).with("name").and_return(nil)
-      subject.should_receive(:puts).with("name does not exist in this safe.")
-      subject.get "name"
-    end
-    it "tells the user the password" do
-      mock_keyring.stub(:get).with("name").and_return("pass")
-      subject.should_receive(:puts).with("name: pass")
-      subject.get "name"
-    end
-  end
-  describe "#remove" do
-    it "removes a key from the keyring" do
-      mock_keyring.should_receive(:remove).with("name")
-      subject.remove("name")
-    end
-    it "displays a KeyMissingException" do
-      mock_keyring.stub(:remove) { raise PasswordSafe::Keyring::KeyMissingException, "dummy msg" }
-      subject.should_receive(:puts).with("dummy msg")
-      subject.remove("name")
-    end
-    it "displays a msg to the user on sucessfull remove" do
-      mock_keyring.stub(:remove)
-      subject.should_receive(:puts).with("entry has been removed")
-      subject.remove("name")
-    end
-  end
-  describe "#change" do
-    it "changes a key in the keyring" do
-      mock_keyring.should_receive(:change).with("name", "pass")
-      subject.change("name", "pass")
-    end
-    it "displays a KeyMissingException" do
-      mock_keyring.stub(:change) { raise PasswordSafe::Keyring::KeyMissingException, "dummy msg" }
-      subject.should_receive(:puts).with("dummy msg")
-      subject.change("name", "pass")
-    end
-    it "displays a msg to the user on sucessfull change" do
-      mock_keyring.stub(:change)
-      subject.should_receive(:puts).with("password for name has been updated")
-      subject.change("name", "pass")
-    end
-  end
-  describe "#list" do
-    it "displays (none) if there are no keys" do
-      mock_keyring.stub(:list).and_return([])
-      subject.should_receive(:puts).with("List: (none)")
-      subject.list
-    end
-    it "displays keys if they exist" do
-      mock_keyring.stub(:list).and_return(["pass1", "pass2"])
-      subject.should_receive(:puts).with("List: pass1, pass2")
-      subject.list
+  context "helpers" do
+    describe "#make_safe" do
+      let(:askable) { mock Object, :ask => "dummy" }
+      before(:each) do
+        HighLine.stub(:new).and_return(askable)
+        PasswordSafe::Safe.stub(:new)
+      end
+      it "initializes a safe" do
+        PasswordSafe::Safe.should_receive(:new)
+        subject.make_safe
+      end
+      it "asks the user for a master safe password" do
+        askable.should_receive(:ask)
+        subject.make_safe
+      end
     end
   end
 end
